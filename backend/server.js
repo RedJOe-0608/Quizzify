38262d9e7f63c53bd1b032cfc921bb459e948dca
jyothi <joeswaroopmakena@gmail.com>
2023-10-02 14:28:48 +0530

    "server.js edited"

--- backend/server.js
+++ backend/server.js
@@ -29,17 +29,20 @@ app.use('/api/users',userRoutes)
 
 if(process.env.NODE_ENV === 'production'){
     // set static folder
-    app.use(express.static(path.join(__dirname,'/frontend/build')))
+    app.use(express.static(path.join(path.resolve(__dirname,'..'),'/frontend/build')))
 
     //any route that is not api will be redirected to index.html
     app.get('*',(req,res) => {
-        res.sendFile(path.resolve(__dirname,"frontend",'build','index.html'))
+        res.sendFile(path.join(path.resolve(__dirname,'..'),'/frontend/build/index.html'))
     })
 }else{
     app.get('/',(req,res) => {
         res.send("API is running...")
     })
 }
+console.log(__filename);
+console.log(path.join(path.resolve(__dirname,'..'),'/frontend/build/index.html'));
+console.log()
 
 app.use(notFound)
 app.use(errorHandler)
