b3b3e4701442ba8c7b75ace038db5e923e5e3833
jyothi <joeswaroopmakena@gmail.com>
2023-10-02 12:14:44 +0530

    "ready to deployment"

--- backend/server.js
+++ backend/server.js
@@ -20,15 +20,27 @@ app.use(express.urlencoded({extended: true}))
 //cookie parser middleware
 app.use(cookieParser())
 
-app.get('/',(req,res) => {
-    res.send("API is running...")
-})
-
 app.use('/api/quizzes',quizRoutes)
 app.use('/api/users',userRoutes)
 
+
 app.use(notFound)
 app.use(errorHandler)
+
+if(process.env.NODE_ENV === 'production'){
+    // set static folder
+    app.use(express.static(path.join(__dirname,'/frontend/build')))
+
+    //any route that is not api will be redirected to index.html
+    app.get('*',(req,res) => {
+        res.sendFile(path.resolve(__dirname,"frontend",'build','index.html'))
+    })
+}else{
+    app.get('/',(req,res) => {
+        res.send("API is running...")
+    })
+}
+
 
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
